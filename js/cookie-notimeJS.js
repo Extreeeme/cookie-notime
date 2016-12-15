ff344a892817f3d60236209b6b03eac3e83af7b3
Extreeeme <martindcontact@gmail.com>
2016-12-15 13:56:53 +0100

    Ajout de la fonction multiplicateur

--- js/cookie-notimeJS.js
+++ js/cookie-notimeJS.js
@@ -5,4 +5,13 @@ var cookies = 0;
 function ajouteUnCookie(){
 	cookies = cookies+multiplicateur;
 	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Juliens";
+	return cookies;
+}
+
+function multiplicateur(cookies){
+	if (cookies >= 20){
+		multiplicateur = multiplicateur * 2;
+		document.getElementById("multiplicateur").innerHTML="Multiplicateur x"+multiplicateur;
+		return multiplicateur;
+	}
 }
