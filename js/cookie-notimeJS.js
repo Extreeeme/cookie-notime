991a3edac31784e8042f2508672d526b9d02ee77
Extreeeme <martindcontact@gmail.com>
2016-12-15 14:12:40 +0100

    Fonction changer image et reset

--- js/cookie-notimeJS.js
+++ js/cookie-notimeJS.js
@@ -4,13 +4,14 @@ var cookies = 0;
 
 function ajouteUnCookie(){
 	cookies = cookies+multiplicateur;
-	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Juliens";
+	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
 	return cookies;
 }
 
 function multiplicateur(cookies){
 	if (cookies >= 20){
 		multiplicateur = multiplicateur * 2;
+		cookies = cookies - 20;
 		document.getElementById("multiplicateur").innerHTML="Multiplicateur x"+multiplicateur;
 		return multiplicateur;
 	}
