5e2bbb883af3cb88ffe6dbb4b40ac66ae191f9ac
Extreeeme <martindcontact@gmail.com>
2016-12-15 12:46:24 +0100

    Réorganisation des dossiers

--- fonction incrémentation cookie/cookie-notime.js
+++
@@ -1,5 +0,0 @@
-function ajouteUnCookie(cookies, multiplicateur){
-	cookies = cookies + (1*multiplicateur);
-	document.getElementById("score").innerHTML = "Nombre de Cookie : "+ cookies;
-	return cookies;
-}
