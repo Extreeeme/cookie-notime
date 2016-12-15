37c29ed1962278fd87a80934d5c951ba4b43d5bf
Extreeeme <martindcontact@gmail.com>
2016-12-15 11:06:15 +0100

    Phrase nombre de cookie

--- fonction incrémentation cookie/cookie-notime.js
+++ fonction incrémentation cookie/cookie-notime.js
@@ -1,4 +1,5 @@
 function ajouteUnCookie(cookies, multiplicateur){
 	cookies = cookies + (1*multiplicateur);
+	document.getElementById("score").innerHTML = "Nombre de Cookie : "+ cookies;
 	return cookies;
 }
