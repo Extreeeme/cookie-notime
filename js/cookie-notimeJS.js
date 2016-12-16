9cc4e1852482c0ecfb2cf90f62817eaa1b5d6750
Extreeeme <martindcontact@gmail.com>
2016-12-16 13:46:52 +0100

    Ajout du cookie

--- js/cookie-notimeJS.js
+++ js/cookie-notimeJS.js
@@ -1,10 +1,21 @@
 var multiplicateur = 1;
 var cookies = 0;
 var prix = 20;
+var highscore = 0;
+
+if ('highscore'){
+	highscore = lireCookie('highscore');
+	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies" + "\n Meilleur score : "+highscore;
+}
 
 document.getElementById("buttonanim").onclick = function (){
 	cookies = cookies+multiplicateur;
-	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
+	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies"+ "\n Meilleur score : "+highscore;
+	if (cookies > highscore){
+		highscore = cookies;
+		document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies" + "\n Meilleur score : "+highscore;
+		saveCookie(highscore);
+	}
 }
 
 document.getElementById("multiplicateur").onclick = function(){
@@ -21,6 +32,22 @@ document.getElementById("reset").onclick = function (){
 	cookies = 0;
 	multiplicateur = 1;
 	prix = 20;
-	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
+	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies" + "\n Meilleur score : "+highscore;
 	document.getElementById("multiplicateur").innerHTML="Multiplicateur\nPrix : 20";
 }
+
+function lireCookie(name){
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function saveCookie(highscore) {
+	document.cookie = 'highscore='+highscore+'; expires=Mon, 1 Mar 2025 00:00:00 UTC; path=/';
+}
+
