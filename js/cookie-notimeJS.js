7e344c08cc6882dd63c83a01c4dbcd027d1f6a32
Extreeeme <martindcontact@gmail.com>
2016-12-16 16:27:52 +0100

    Ajout du paragraphe High Score

--- js/cookie-notimeJS.js
+++ js/cookie-notimeJS.js
@@ -5,15 +5,18 @@ var highscore = 0;
 
 if ('highscore'){
 	highscore = lireCookie('highscore');
-	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies" + "\n Meilleur score : "+highscore;
+	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
+	document.getElementById("highscore").innerHTML="Meilleur score : "+highscore;
 }
 
 document.getElementById("buttonanim").onclick = function (){
 	cookies = cookies+multiplicateur;
-	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies"+ "\n Meilleur score : "+highscore;
+	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
+	document.getElementById("highscore").innerHTML="Meilleur score : "+highscore;
 	if (cookies > highscore){
 		highscore = cookies;
-		document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies" + "\n Meilleur score : "+highscore;
+		document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
+		document.getElementById("highscore").innerHTML="Meilleur score : "+highscore;
 		saveCookie(highscore);
 	}
 }
@@ -25,6 +28,7 @@ document.getElementById("multiplicateur").onclick = function(){
 		prix = prix*2;
 		document.getElementById("multiplicateur").innerHTML="Multiplicateur +" + multiplicateur + "\nPrix : "+prix;
 		document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
+		document.getElementById("highscore").innerHTML="Meilleur score : "+highscore;
 	}
 }
 
@@ -32,7 +36,8 @@ document.getElementById("reset").onclick = function (){
 	cookies = 0;
 	multiplicateur = 1;
 	prix = 20;
-	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies" + "\n Meilleur score : "+highscore;
+	document.getElementById("score").innerHTML="Vous avez : "+cookies+" Cookies";
+	document.getElementById("highscore").innerHTML="Meilleur score : "+highscore;
 	document.getElementById("multiplicateur").innerHTML="Multiplicateur\nPrix : 20";
 }
 
@@ -44,10 +49,10 @@ function lireCookie(name){
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
 		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
 	}
-	return null;
+	return 0;
 }
 
 function saveCookie(highscore) {
-	document.cookie = 'highscore='+highscore+'; expires=Mon, 1 Mar 2025 00:00:00 UTC; path=/';
+	document.cookie = "highscore="+highscore+"; expires=Mon, 1 Mar 2025 00:00:00 UTC; path=/";
 }
 
